8c8036de2ab1b1194c88123cc4fdd75287457a63
zhangzhanyi <zhangzhanyi@i-focusing.com>
2019-07-08 17:03:38 +0800

    *#;添加自定义json解析器代替litjson,修复webgl下litjson不兼容的错误

--- Assets/2048/Script/JavaScriptObject/Script/CrossPlatform/JavaScriptObjectFactory.cs
+++ Assets/2048/Script/JavaScriptObject/Script/CrossPlatform/JavaScriptObjectFactory.cs
@@ -2,23 +2,16 @@
 {
     public class JavaScriptObjectFactory
     {
-        private static IJsonString2JavaScriptObjectHandle jsonString2JavaScriptObject = null;
+        private static IJsonString2JavaScriptObjectHandle jsonParse = null;
 
         static JavaScriptObjectFactory()
         {
-#if UNITY_STANDALONE_WIN || UNITY_IPHONE || UNITY_ANDROID || UNITY_EDITOR
-            jsonString2JavaScriptObject = new JavaScriptObjectWithLitJson();
-
-#elif UNITY_UWP
-        jsonString2JavaScriptObject = new JavaScriptObjectWithUWP();
-#else
-        throw new System.Exception("该平台暂不支持");
-#endif
+            jsonParse = new JavaScriptObjectWithJsonParse();
         }
 
         public static JavaScriptObject CreateJavaScriptObject(string json)
         {
-            return jsonString2JavaScriptObject.ToJavaScriptObject(json);
+            return jsonParse.ToJavaScriptObject(json);
         }
     }
 
